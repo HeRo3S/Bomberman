73af76dbb316f0e59f87a988aa29803553cfd417
ducanh261101a <71822231+ducanh261101a@users.noreply.github.com>
2020-11-21 15:42:42 +0700

    Fix Load Sprite

--- src/Sprite.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Sprite {
-    private String filepath = "/resources/Sprite4.png";
-    private Spritesheet spritesheet = new Spritesheet(filepath,32,32,12,12);
-
-    public Sprite() throws IOException {
-    }
-}
